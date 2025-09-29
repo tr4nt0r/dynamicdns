dc062d4296fbba6da7b00d86c6661154f278e0e1
tr4nt0r <4445816+tr4nt0r@users.noreply.github.com>
2025-09-29 06:46:34 +0200

    Bump version 0.1.0rc0 → 0.1.0rc1

--- src/dynamicdns/__init__.py
+++ src/dynamicdns/__init__.py
@@ -5,7 +5,7 @@
 from .providers import Provider, ProviderConf, providers
 from .updater import Updater
 
-__version__ = "0.1.0rc0"
+__version__ = "0.1.0rc1"
 __all__ = [
     "providers",
     "Provider",
